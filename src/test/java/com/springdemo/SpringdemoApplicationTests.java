77880624a8c07344165c6344fd448745b4671986
ranjith413 <117443891+ranjith413@users.noreply.github.com>
2023-01-20 11:23:51 +0530

    Update SpringdemoApplicationTests.java

--- src/test/java/com/springdemo/SpringdemoApplicationTests.java
+++ src/test/java/com/springdemo/SpringdemoApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringdemoApplicationTests {
 
-	@Test
+	@Test(expected = Test.None.class)
 	void contextLoads() {
 	}
 
